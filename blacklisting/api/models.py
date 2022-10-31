3ba7be836f74436d3c42ae1b9809b950d25c0757
Anant Nema <anantnema@gmail.com>
2022-10-31 22:27:37 +0530

    Adding for ipv6

--- blacklisting/api/models.py
+++ blacklisting/api/models.py
@@ -5,3 +5,9 @@ class Ipv4(models.Model):
 
     def __str__(self):
         return self.ip
+
+class Ipv6(models.Model):
+    ip = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.ip
